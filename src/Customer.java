ade209bc251772502f7eb41fe2e0301e86b0748c
Xia-Minghong <mxia001@e.ntu.edu.sg>
2014-10-25 21:05:04 +0800

    who is quanA

---
+++ src/Customer.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Xia-Minghong on 14-10-25.
+ */
+public class Customer {
+
+}
---
+++ src/Table.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Xia-Minghong on 14-10-25.
+ */
+public class Table {
+    int TABLE_ID;
+    int CAPACITY;
+    boolean presence;
+
+    public int getTABLE_ID() {
+
+        return TABLE_ID;
+    }
+
+    public int getCAPACITY() {
+        return CAPACITY;
+    }
+
+    public boolean isPresence() {
+        return presence;
+    }
+
+    public void setPresence(boolean presence) {
+        this.presence = presence;
+    }
+}
