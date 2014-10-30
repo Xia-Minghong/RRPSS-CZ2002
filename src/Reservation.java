6ce1db7e3401df2ddea4b64839d86fd3a3d71b2a
雨谋 <@Lenovo-PC>
2014-10-30 14:41:46 +0800

    reservation

---
+++ src/Reservation.java
@@ -0,0 +1,40 @@
+package project;
+
+public class Reservation {
+	private String date;
+	private String time;
+	private int pax;
+	private Member customer;
+	
+	public Reservation(String date, String time, int pax, Member customer){
+		this.setDate(date);
+		this.setTime(time);
+		this.setPax(pax);
+		this.setCustomer(customer);
+	}
+	public String getDate(){
+		return this.date;
+	}
+	public void setDate(String date){
+		this.date = date;
+	}
+	public String getTime(){
+		return this.time;
+	}
+	public void setTime(String time){
+		this.time = time;
+	}
+	public int getPax(){
+		return pax;
+	}
+	public void setPax(int pax){
+		this.pax = pax;
+	}
+	public Member getCustomer(){
+		return this.customer;
+	}
+	public void setCustomer(Member customer){
+		this.customer = customer;
+	}
+
+}
